a0660f8e2eafc99b1b709efe30c17f21c335d1ca
Rami Al-Muhana <ramealmuhana@hotmail.com>
2024-09-21 17:19:36 +0200

    De map aangepast zodat het op obstacles game lijktlijkt 1

--- ObstacleAssault/Source/ObstacleAssault/MovingPlatform.cpp
+++ ObstacleAssault/Source/ObstacleAssault/MovingPlatform.cpp
@@ -15,7 +15,10 @@ AMovingPlatform::AMovingPlatform()
 void AMovingPlatform::BeginPlay()
 {
 	Super::BeginPlay();
-	
+
+	StartLocation = GetActorLocation();
+
+	UE_LOG(LogTemp, Display, TEXT("Your message"));
 }
 
 // Called every frame
@@ -23,5 +26,43 @@ void AMovingPlatform::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
+	MovePlatform(DeltaTime);
+	RotatePlatform(DeltaTime);
 }
 
+
+void AMovingPlatform::MovePlatform(float DeltaTime)
+{
+	
+	if (ShouldPlatformReturn())
+	{
+		FVector MoveDirection = PlatformVelocity.GetSafeNormal();
+		StartLocation = StartLocation + MoveDirection * MoveDistance;
+		SetActorLocation(StartLocation);
+		PlatformVelocity = -PlatformVelocity;
+	}
+	else
+	{
+		FVector CurrentLocation = GetActorLocation();
+		CurrentLocation = CurrentLocation + (PlatformVelocity * DeltaTime);
+		SetActorLocation(CurrentLocation);
+	}
+	
+}
+
+
+void AMovingPlatform::RotatePlatform(float DeltaTime)
+{
+	AddActorLocalRotation(RotationVelocity * DeltaTime);
+}
+
+
+bool AMovingPlatform::ShouldPlatformReturn() const
+{
+	return GetDistanceMoved() > MoveDistance;
+}
+
+float AMovingPlatform::GetDistanceMoved() const
+{
+	return FVector::Dist(StartLocation, GetActorLocation());
+}
